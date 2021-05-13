24113a9418266dd8ad19c8d027af3c48bdb132b4
makhatadze <vitomaxatadze@gmail.com>
2021-05-13 15:58:29 +0400

    Make navigation for clients with links and routes.

---
+++ resources/views/clients.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Clients') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    Here are a list of your clients
+                </div>
+                @foreach($clients as $client)
+                    <div class="py-3 text-gray-900">
+                        <h3 class="text-lg text-gray-500">
+                            {{ $client->name }}
+                        </h3>
+                        <p>
+                            {{ $client->redirect }}
+                        </p>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,4 +22,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::get('/dashboard/clients', function (Request $request) {
+    return view('clients', [
+        'clients' => $request->user()->clients
+    ]);
+})->middleware(['auth'])->name('dashboard.clients');
+
+require __DIR__ . '/auth.php';
